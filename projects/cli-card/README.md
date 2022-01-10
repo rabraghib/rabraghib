910f4462f3f5cda7fcad9381f5af69a06d87651e
Rabyâ Raghib <rabraghib@gmail.com>
2022-01-10 23:16:37 +0100

    feat: :sparkles: add cli-card
    #1

---
+++ projects/cli-card/README.md
@@ -0,0 +1,13 @@
+# Rabyâ Raghib (@rabraghib) Cli Card
+
+## Usage
+
+```
+npx rabraghib
+```
+
+## Thanks to
+
+- [@chihab](https://github.com/chihab) for the using it 🤷‍♂️
+- [@bnb](https://github.com/bnb) for the initial idea.
+- [@swyx](https://github.com/sw-yx) for talking about it.
---
+++ projects/cli-card/bin/cli.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+// 👆 Used to tell Node.js that this is a CLI tool
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const output = fs.readFileSync(path.join(__dirname, 'output'), 'utf8');
+console.log(output.replace(/<!--.*?-->/gs, ''));
+// 👆 Remove comments from the output
