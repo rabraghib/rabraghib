516231ed8ebb9cb2da0324bdba0d4b9d5df41fad
Rabyâ Raghib <rabraghib@gmail.com>
2022-01-14 16:21:06 +0100

    feat: :rocket: add `Tech Stack` section in the README

--- content/data/_index.js
+++ content/data/_index.js
@@ -7,7 +7,13 @@ const projects = require('./projects.json');
 
 module.exports = {
   AboutData: about,
-  BlogData: blog,
+  BlogData: {
+    ...blog,
+    posts: blog.posts.map(post => ({
+      ...post,
+      url: post.publishedOn['hashnode'] || Object.values(post.publishedOn)[0]
+    }))
+  },
   BrandData: brand,
   ProjectsData: projects
 };
--- tools/build-content/prepare-data.js
+++ tools/build-content/prepare-data.js
@@ -3,7 +3,10 @@ const cliCardContent = require('./cli-card-content');
 
 const ReadmeIncludedProfiles = ['twitter', 'instagram', 'linkedin'];
 const mailBadge = {
-  badge: getBadgeUrl('Mail me!', 'mail'),
+  badge: getBadgeUrl({
+    label: 'Mail me!',
+    logo: 'mail'
+  }),
   url: `mailto:${DATA.AboutData.email}`,
   alt: 'Mail me!'
 };
@@ -16,12 +19,25 @@ module.exports = {
       .filter(p => ReadmeIncludedProfiles.includes(p.platform))
       .map(p => {
         return {
-          badge: getBadgeUrl(p.platform),
+          badge: getBadgeUrl({
+            label: p.platform
+          }),
           url: p.url,
           alt: `${DATA.AboutData.name} ${p.platform} profile`
         };
       })
   ],
+  ReadmeTechStackBadges: DATA.AboutData.stack.map(tech => {
+    return {
+      badge: getBadgeUrl({
+        label: tech,
+        color: getColor('slate-900'),
+        logoColor: getColor('slate-50'),
+        labelColor: getColor('primary')
+      }),
+      alt: `${tech}`
+    };
+  }),
   GithubStatsCardsUrls: [
     `https://github-readme-streak-stats.herokuapp.com/?${getQueryParamsOf(
       'streak-stats'
@@ -39,12 +55,23 @@ module.exports = {
 function getColor(colorName) {
   return DATA.BrandData.colors[colorName].replace('#', '');
 }
-function getBadgeUrl(label, logo = label) {
-  return `https://img.shields.io/badge/${encodeURI(label)}--${getColor(
-    'primary'
-  )}?style=for-the-badge&logo=${encodeURI(logo)}&logoColor=${getColor(
-    'slate-50'
-  )}&logoWidth=25&labelColor=${getColor('slate-900')}`;
+function getBadgeUrl(options = {}) {
+  const [color, logoColor, labelColor, style] = [
+    options.color ?? getColor('primary'),
+    options.logoColor ?? getColor('slate-50'),
+    options.labelColor ?? getColor('slate-900'),
+    options.style ?? 'for-the-badge'
+  ];
+  options = {
+    label: options.label ?? '',
+    message: options.message ?? '',
+    logo: options.logo ?? options.label ?? ''
+  };
+  return `https://img.shields.io/badge/${encodeURI(options.label)}-${
+    options.message
+  }-${color}?style=${style}&logo=${encodeURI(
+    options.logo
+  )}&logoColor=${logoColor}&labelColor=${labelColor}`;
 }
 function setValue(obj, props, value) {
   while (props.length > 0) obj[props.pop()] = value;
